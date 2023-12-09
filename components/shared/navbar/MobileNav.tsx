df3e7359906b68cf1fdb1bbcfe9c1316547ca035
Mox90 <moctar.sandalo89@gmail.com>
2023-12-09 18:12:13 +0300

    Implementing LefSidebar

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -16,7 +16,7 @@ import { usePathname } from 'next/navigation';
 const NavContent = () => {
   const pathname = usePathname();
   return (
-    <section className='flex h-full flex-col gap-6 pt-16'>
+    <section className='flex h-full flex-col gap-6 pt-10'>
       {sidebarLinks.map((link) => {
         const isActive =
           (pathname.includes(link.route) && link.route.length > 1) ||
@@ -30,7 +30,7 @@ const NavContent = () => {
                 isActive
                   ? 'primary-gradient rounded-lg text-light-900'
                   : 'text-dark300_light900'
-              } flex items-center justify-start gap-4 bg-transparent p-4`}
+              } flex items-center justify-start gap-4 rounded-r-full bg-transparent p-4`}
             >
               <Image
                 src={link.imgURL}
