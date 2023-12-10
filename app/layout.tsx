1b7dfefc389ce38e0439aee9e684699e41ee93ec
Mox90 <moctar.sandalo89@gmail.com>
2023-12-10 09:28:42 +0300

    Implementing Rightsidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' dir='ltr'>
       <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
         <ClerkProvider
           appearance={{
